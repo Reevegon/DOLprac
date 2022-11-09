6c8bd7c9147cce100efe58545e9af4d53e263a17
Reevegon <96007460+Reevegon@users.noreply.github.com>
2022-11-09 21:24:47 +0530

    Update Tp.java

--- Tp.java
+++ Tp.java
@@ -1,11 +1,25 @@
-public class B {
-	public static void main(String[] args) {
-		System.out.println("Enter the name:"+args[0]);
-		System.out.println("Enter the surname:"+args[1]);
+class Main
+{
+        public static void main(String args[])
+	{
+
+	int a = Integer.parseInt(args[0]); 
+	int b = Integer.parseInt(args[1]);
+	int c = 0;
+	
+		System.out.println("First no. is: "+args[0]); 
+		System.out.println("Second no. is: "+args[1]);
+
+	c=a-b;
+
+	System.out.println("substraction of two no. is: "+c); 
 	}
+
 }
 
 
-cd C:\Users\91985\OneDrive\Documents
-javac B.java
-java B %reeve% %gonsalves% 
+
+
+cd C:\exams
+javac Main.java
+java Main "%Num1%" "%Num2%" 
